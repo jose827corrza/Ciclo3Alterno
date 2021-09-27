02d4bb91bc82def324dfcd5d71061258c286e776
Jose <jose96corrza@gmail.com>
2021-09-27 03:10:00 -0500

    Se anexa tarjeta devs. y carousel

--- accionJS.js
+++ accionJS.js
@@ -1,53 +1,40 @@
-function my(){
-    let emailText = document.getElementById('exampleInputEmail1').value;
-    let psswrd = document.getElementById('exampleInputPassword1').value;
-    alert("datos "+emailText+psswrd);
+//import getAuth from "./node_modules/@firebase";
+// import {get} from 'firebase/auth';
+// const signupform = document.getElementById('form-group');
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA12E98UfraYKcCFxIoC8QyRooXnlumn1A",
+//   authDomain: "ciclo3misiontic-b9088.firebaseapp.com",
+//   projectId: "ciclo3misiontic-b9088",
+//   storageBucket: "ciclo3misiontic-b9088.appspot.com",
+//   messagingSenderId: "953197985528",
+//   appId: "1:953197985528:web:8e5b859b8f4e488dd4050d",
+//   measurementId: "G-QYRQFL4T7G"
+// };
+// firebaseConfig.auth().createUserWithEmailAndPassword()
+// if(signupform){
+//   signupform.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   console.log('enviando')
+//   const signupEmail = document.getElementById('NewUserEmail').value;
+//   const signupPassword = document.getElementById('NewUserPassword').value;
+//   console.log(signupEmail,signupPassword);
+//   })
+// }
+// var auth = getAuth(firebaseConfig);
+// const provider = new GoogleAuthProvider();
+// createUserWithEmailAndPassword(auth,'jose@correo.com','7326919')
+//   .then(userCredential =>{
+//     console.log('registrado')
+//   })
+
+
+// function my(){
+//     let emailText = document.getElementById('#NewUserEmail').value;
+//     let psswrd = document.getElementById('exampleInputPassword1').value;
+//     alert("datos "+emailText+psswrd);
     
-}
-var  firebaseConfig = {
-    apiKey: "AIzaSyA12E98UfraYKcCFxIoC8QyRooXnlumn1A",
-    authDomain: "ciclo3misiontic-b9088.firebaseapp.com",
-    projectId: "ciclo3misiontic-b9088",
-    storageBucket: "ciclo3misiontic-b9088.appspot.com",
-    messagingSenderId: "953197985528",
-    appId: "1:953197985528:web:95ebe892f090b359d4050d",
-    measurementId: "G-P18NE6S3XT"
-  };
+    
+// }
+//const auth = firebase.getAuth();
 
-firebase.initializeApp(firebaseConfig);
-    const auth = firebase.auth();
-    auth.languageCode = "es";
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-
-// Login con un proveedor
-async function loginProvider(provider) {
-    const response = await auth.signInWithPopup(provider)
-    console.log(response);
-    }
-
-loginProvider(googleProvider);
-async function createWithEmailAndPassword(emailText, psswrd){
-    try {
-        const response = await auth.createUserWithEmailAndPassword(emailText, psswrd);
-        var user = firebase.auth().currentUser;
-        console.log(user);
-        console.log(response.user.exampleInputEmail1);
-      } catch (error) {
-        throw new Error(error)
-      }
-}
-createWithEmailAndPassword('prueba@email.com', '123456')
-
-// Login usuario email y password
-async function signInWithEmailAndPassword(emailText, psswrd) {
-    try {
-      const response = await auth.signInWithEmailAndPassword(emailText, psswrd);
-      const user = firebase.auth().currentUser;
-      console.log(user);
-      console.log(response.user.emailText);
-      actualStatusAuth()
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-  signInWithEmailAndPassword('prueba@email.com', '123456')
